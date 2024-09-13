6fb7ba48fc9b4496a3ab167b8446d92639968121
fred <fredjuma8@gmail.com>
2024-09-14 01:26:44 +0300

    setting up the editor

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,16 @@
-
 import { Editor } from '@/components/editor/Editor'
+import Header from '@/components/header'
 import React from 'react'
 
 const Document = () => {
   return (
     <div>
+      <Header>
+        <div className='flex w-fit item-center justify-center gap-2'>
+          <p className='document-title'>document title</p>
+        </div>
+      </Header>
+       
         <Editor />
     </div>
   )
