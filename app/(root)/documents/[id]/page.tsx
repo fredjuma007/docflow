6d778af8c267a2f64bdc624a506e1bc03d2a4b46
fred <fredjuma8@gmail.com>
2024-09-14 02:06:11 +0300

    authentication with clerk

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -8,7 +8,7 @@ const Document = () => {
     <div>
       <Header>
         <div className='flex w-fit item-center justify-center gap-2'>
-          <p className='document-title'>document title</p>
+          <p className='document-title'>Share</p>
         </div>
         <SignedOut>
             <SignInButton />
