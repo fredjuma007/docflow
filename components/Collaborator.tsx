609279fd11eeb30608ee03acc4cca163a2b97eb2
fred <fredjuma8@gmail.com>
2024-09-25 04:22:51 +0300

    user permissions

--- components/Collaborator.tsx
+++ components/Collaborator.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import React, { useState } from 'react'
 import UserTypeSelector from './UserTypeSelector';
 import { Button } from './ui/button';
+import { removeCollaborator, updateDocumentAccess } from '@/lib/actions/room.actions';
 
 const Collaborator = ({ roomId, creatorId, collaborator, email, user }
     : CollaboratorProps ) => {
@@ -9,8 +10,25 @@ const Collaborator = ({ roomId, creatorId, collaborator, email, user }
             'viewer');
         const [loading, setLoading] = useState(false);
 
-        const shareDocumentHandler = async (type: string) => {}
-        const removeCollaboratorHandler = async (email: string) => {}
+        const shareDocumentHandler = async (type: string) => {
+            setLoading(true);
+
+            await updateDocumentAccess({ 
+                roomId, 
+                email, 
+                userType: type as UserType, 
+                updatedBy: user
+            })
+
+            setLoading(false);
+        }
+        const removeCollaboratorHandler = async (email: string) => {
+            setLoading(true);
+
+            await removeCollaborator({ roomId, email });
+
+            setLoading(false);
+        }
   return (
     <li className='flex items-center justify-between gap-2 py-3'>
         <div className='flex gap-2'>
