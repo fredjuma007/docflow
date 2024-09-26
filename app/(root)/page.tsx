2b1f81c5aa80af66d1b4d05d750264293ce0e84d
fred <fredjuma8@gmail.com>
2024-09-26 04:27:42 +0300

    notifications

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import AddDocumentBtn from '@/components/AddDocumentBtn'
 import { DeleteModal } from '@/components/DeleteModal'
 import Header from '@/components/header'
+import Notification from '@/components/Notification'
 import { Button } from '@/components/ui/button'
 import { getDocuments } from '@/lib/actions/room.actions'
 import { dateConverter } from '@/lib/utils'
@@ -21,7 +22,7 @@ const Home = async () => {
    <main className='home-container'>
       <Header className='sticky left-0 top-0'>
         <div className='flex item-center gap-2 lg:gap-4'>
-              Notification
+              <Notification />
               <SignInButton>
                 <UserButton />
               </SignInButton>
